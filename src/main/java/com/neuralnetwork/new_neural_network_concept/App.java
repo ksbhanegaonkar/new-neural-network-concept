91fc9c29c10ef73f9208d00fa6d33085e5f7cd8b
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2020-03-07 10:04:39 +0530

    initial commit

---
+++ src/main/java/com/neuralnetwork/new_neural_network_concept/App.java
@@ -0,0 +1,13 @@
+package com.neuralnetwork.new_neural_network_concept;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
